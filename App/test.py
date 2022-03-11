aa97c69437e613a95a2f03c8104ee0554c2e545a
usamaali012 <63456918+usamaali012@users.noreply.github.com>
2022-03-11 11:05:17 +0500

    Update test.py
    Updated

--- App/test.py
+++ App/test.py
@@ -7,7 +7,7 @@ print(int(ts))
 dt_object = datetime.fromtimestamp(time.time())
 print(dt_object)
 
-sub = '106368296827960270379'
+sub = 'enter sub here'
 
 """    <div><fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
     </fb:login-button></div> """
